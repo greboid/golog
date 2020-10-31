668a2cfd8c0a34ebeaaa5056fca0dc10b7d40587
Greg Holmes <git@greg.holmes.name>
2020-10-31 15:31:16 +0000

    Add a tiny amount of godoc

--- log.go
+++ log.go
@@ -5,6 +5,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+//Creates a new SugaredLogger and panics if it couldn't be created
 func MustCreateLogger(debug bool) *zap.SugaredLogger {
 	logger, err := CreateLogger(debug)
 	if err != nil {
@@ -13,6 +14,10 @@ func MustCreateLogger(debug bool) *zap.SugaredLogger {
 	return logger
 }
 
+//Creates a new SugaredLogger, an error is returned if the creation fails
+//
+//The debug parameters controls how much logging is displayed.  It also controls if the caller and stack trace are shown
+//on error
 func CreateLogger(debug bool) (*zap.SugaredLogger, error) {
 	zapConfig := zap.NewDevelopmentConfig()
 	zapConfig.DisableCaller = !debug
