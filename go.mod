0d0c37d0c3e49d2b1548b551b65363f5793bee29
Greg Holmes <git@greg.holmes.name>
2022-02-03 13:53:30 +0000

    Update and tidy

--- go.mod
+++ go.mod
@@ -1,7 +1,11 @@
-module github.com/greboid/go-log
+module github.com/greboid/golog
 
-go 1.15
+go 1.17
+
+require go.uber.org/zap v1.20.0
 
 require (
-	go.uber.org/zap v1.10.0
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
 )
--- log.go
+++ log.go
@@ -5,7 +5,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-//Creates a new SugaredLogger and panics if it couldn't be created
+// MustCreateLogger Creates a new SugaredLogger and panics if it couldn't be created
 func MustCreateLogger(debug bool) *zap.SugaredLogger {
 	logger, err := CreateLogger(debug)
 	if err != nil {
@@ -14,10 +14,10 @@ func MustCreateLogger(debug bool) *zap.SugaredLogger {
 	return logger
 }
 
-//Creates a new SugaredLogger, an error is returned if the creation fails
+// CreateLogger Creates a new SugaredLogger, an error is returned if the creation fails
 //
-//The debug parameters controls how much logging is displayed.  It also controls if the caller and stack trace are shown
-//on error
+// The debug parameters controls how much logging is displayed.  It also controls if the caller and stack trace are shown
+// on error
 func CreateLogger(debug bool) (*zap.SugaredLogger, error) {
 	zapConfig := zap.NewDevelopmentConfig()
 	zapConfig.DisableCaller = !debug
